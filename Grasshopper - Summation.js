811651648db824e810f1a4976a5072ac9157cbad
dastree <98938815+dastree@users.noreply.github.com>
2022-04-30 03:42:58 -0500

    daily kata backup

---
+++ Grasshopper - Summation.js
@@ -0,0 +1,12 @@
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+var summation = function (num) {
+    let i = 1;
+    let sum = 0;
+    while (i <= num)
+      {
+        sum = sum + i;
+        i++;
+      } return sum
+    // Code here
+  }
