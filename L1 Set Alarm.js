611cac35915076a5b8ba2fd76b6d645f2fcb3caf
dastree <98938815+dastree@users.noreply.github.com>
2022-05-01 12:54:10 -0500

    daily old kata back up

---
+++ L1 Set Alarm.js
@@ -0,0 +1,14 @@
+/*Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise.
+*/
+
+function setAlarm(employed, vacation){
+    if ((employed == true) && (vacation == false)){ 
+      console.log('set alarm')
+      return true
+    } else if ((employed == false) || (vacation == true)) {
+      console.log('no alarm today')
+      return false
+    }
+  }
