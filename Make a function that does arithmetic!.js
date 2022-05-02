14ed2ef353812c527ad8e60631128aa2e624a5e0
dastree <98938815+dastree@users.noreply.github.com>
2022-05-02 03:12:09 -0500

    changed code layout

--- Make a function that does arithmetic!.js
+++ Make a function that does arithmetic!.js
@@ -3,12 +3,17 @@
 a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
 
 The four operators are "add", "subtract", "divide", "multiply".
+
 */
 
 function arithmetic(a, b, operator){
-    if (operator == 'add'){return (a + b);    
-     } else if (operator == 'subtract'){return (a - b);
-     } else if (operator == 'multiply'){return (a * b);
-     } else if (operator == 'divide'){return (a / b);
+    if (operator == 'add'){
+      return (a + b);    
+     } else if (operator == 'subtract'){
+       return (a - b);
+     } else if (operator == 'multiply'){
+       return (a * b);
+     } else if (operator == 'divide'){
+       return (a / b);
      } 
    }
