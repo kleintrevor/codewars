6382afb1167f15d4481165a11e0b2d71c6049403
dastree <98938815+dastree@users.noreply.github.com>
2022-05-08 15:28:03 -0500

    doubles the values of the array and returns them as a new array

---
+++ lost without a map.js
@@ -0,0 +1,14 @@
+/* 
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+*/
+
+function maps(x){
+const arr = x.map(x => x * 2);
+return arr;
+}
