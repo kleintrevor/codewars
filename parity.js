1fae34ca212ae9ab599cbad4819cd319ab1c3b4e
kleintrevor <98938815+kleintrevor@users.noreply.github.com>
2022-05-24 14:30:00 -0600

    checks parity

---
+++ parity.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+for (let i = 1; i <= 10; i++) {
+  if (i % 3 === 0) {
+    console.log(`${i} is odd`);
+  }
+}
+
+
+let input = prompt("enter a number between 1 & 10")
+
+for (let i = input; i <= 10; i++) {
+  if (i % 3 === 0) {
+    console.log(`${i} is odd`);
+  }
+}
