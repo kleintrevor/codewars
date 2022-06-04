1486218c5f75ea70a54f2bfe33a2223a841373f0
kleintrevor <98938815+kleintrevor@users.noreply.github.com>
2022-06-04 00:52:05 -0500

    debug to fix the error

---
+++ basic variable assignment.js
@@ -0,0 +1,11 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+// *****Debug this code*****
+/*  
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+*/
+
+const a = "code";
+const b = "wa.rs";
+let name = `${a}${b}`;
