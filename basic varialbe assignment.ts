7112c34c6c479ae7c03381e8af373c022428acfc
kleintrevor <98938815+kleintrevor@users.noreply.github.com>
2022-06-10 14:27:00 -0600

    debug for basic variable issues

---
+++ basic varialbe assignment.ts
@@ -0,0 +1,13 @@
+/*
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+***** BUG *****
+var a == "code";
+var b == "wa.rs";
+export var name == a + b;
+
+*/
+
+var a = "code";
+var b = "wa.rs";
+export var name = a + b;
