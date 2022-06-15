e40c3edb9610c924b2bce915aa692e0b2dcfc0c4
kleintrevor <98938815+kleintrevor@users.noreply.github.com>
2022-06-15 03:17:00 -0600

    add i++ to correct the loop

--- Unfinished Loop - Bug Fixing 1.js
+++ Unfinished Loop - Bug Fixing 1.js
@@ -2,8 +2,7 @@
 Unfinished Loop - Bug Fixing #1
 Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
 
-        ***** BUG *****
-        
+***** BUG *****
 function createArray(number){
   var newArray = [];
   
@@ -16,11 +15,11 @@ function createArray(number){
 */
 
 function createArray(number){
-    var newArray = [];
-    
-    for(var counter = 1; counter <= number; counter++){
-      newArray.push(counter);
-    }
-    
-    return newArray;
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
   }
+  
+  return newArray;
+}
